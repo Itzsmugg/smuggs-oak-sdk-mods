5537990bd414585c4826521cdfc596037594cc41
Itzsmugg <93800038+Itzsmugg@users.noreply.github.com>
2023-11-19 00:02:42 +0000

    Update __init__.py

--- add_to_inv/__init__.py
+++ add_to_inv/__init__.py
@@ -1,25 +1,26 @@
-import mods_base
+# Reordered imports
 from argparse import Namespace
-from mods_base import build_mod, command, Game, get_pc
+import mods_base
 
+# Ensuring the SDK version is up-to-date
 assert mods_base.__version_info__ >= (1, 0), "Please update the SDK"
 
+# Version information
 __version__: str
 __version_info__: tuple[int, ...]
 
-
-@command("add_to_inv", 
-        description="Adds the \"add_to_inv\" command to add a serial code from a save editor.")
+@command("add_to_inv", description="Adds the \"add_to_inv\" command to add a serial code from a save editor.")
 def add_to_inv(args: Namespace) -> None:
-    """Adds the specified serial code to the player's inventory.
+    """
+    Adds the specified serial code to the player's inventory.
 
     Args:
         args (Namespace): The parsed command-line arguments.
     """
-    
     pc = get_pc()
     serial_code = args.serial_code
 
+    # Extracting serial code if wrapped in parentheses
     if "(" in serial_code and ")" in serial_code:
         start = serial_code.find("(") + 1
         end = serial_code.find(")")
@@ -27,6 +28,7 @@ def add_to_inv(args: Namespace) -> None:
 
     print(f"Adding serial code {serial_code} to inventory")
 
+    # Adding serial code based on the game
     if Game.get_current() is Game.BL3:
         pc.ServerAddGearToInventory(serial_code, 0)
     elif Game.get_current() is Game.WL:
@@ -34,7 +36,8 @@ def add_to_inv(args: Namespace) -> None:
 
     print("Successfully added to inventory")
 
-
+# Adding argument description for serial_code
 add_to_inv.add_argument("serial_code", help="Params are `add_to_inv SERIALCODE FROM SAVE EDITOR`")
 
+# Building the mod
 build_mod()
