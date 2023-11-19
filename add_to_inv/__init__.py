ffdac418da03fe93412de17de8144410e01a1659
Itzsmugg <93800038+Itzsmugg@users.noreply.github.com>
2023-11-19 00:05:07 +0000

    Update __init__.py

--- add_to_inv/__init__.py
+++ add_to_inv/__init__.py
@@ -1,17 +1,24 @@
 import mods_base
 from argparse import Namespace
-from mods_base import build_mod, command, Game, get_pc
 
-assert mods_base.__version_info__ >= (1, 0), "Please update the SDK"  
+from mods_base import (
+    build_mod, 
+    command, 
+    Game, 
+    get_pc
+)
+
+assert mods_base.__version_info__ >= (1, 0), "Please update the SDK"
 
 __version__: str
 __version_info__: tuple[int, ...]
 
 
-@command("add_to_inv", 
-        description="Adds the \"add_to_inv\" command to add a serial code from a save editor.")
+@command("add_to_inv",
+        description="Adds the \"add_to_inv\" command to add a serial code from a save editor.")  
 def add_to_inv(args: Namespace) -> None:
-    """Adds the specified serial code to the player's inventory.
+    """
+    Adds the specified serial code to the player's inventory.
 
     Args:
         args (Namespace): The parsed command-line arguments.
@@ -33,6 +40,7 @@ def add_to_inv(args: Namespace) -> None:
         pc.ServerAddGearToInventory(serial_code, 0, 0)
 
     print("Successfully added to inventory")
+
 
 add_to_inv.add_argument("serial_code", help="Params are `add_to_inv SERIALCODE FROM SAVE EDITOR`")
 
